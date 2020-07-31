f893310e0f12b981733052fbd570dfc5dfa82422
Kushagra <kush12092001@gmail.com>
2020-07-31 22:19:26 +0530

    Store.js updated

--- Gsoc-Project Web/client/src/store.js
+++ Gsoc-Project Web/client/src/store.js
@@ -1,17 +1,16 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
